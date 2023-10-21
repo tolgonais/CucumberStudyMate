83a60c3e961abb798a7e387ee48016221dd95ffb
Your Name <you@example.com>
2023-10-20 20:59:21 -0700

    courseTestChange

--- src/test/java/steps/CourseTest.java
+++ src/test/java/steps/CourseTest.java
@@ -8,19 +8,23 @@ public class CourseTest {
 
     @Given("the user is on Course page")
     public void the_user_is_on_course_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
+
     @When("click on create")
     public void click_on_create() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
+    @When("provides name,date, time")
+    public void provides_name_date_time() {
+
+    }
+
     @Then("verify that the Course has been successfully created")
     public void verify_that_the_course_has_been_successfully_created() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
+
 
 
 }
